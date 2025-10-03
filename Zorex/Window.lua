3a0e2db4a7ef0e507909e5ed101e2396016ee3ed
Zoprever <zoprever@gmail.com>
2025-10-03 03:44:12 -0700

    Update Window.lua

--- Zorex/Window.lua
+++ Zorex/Window.lua
@@ -1,5 +1,3 @@
--- Universal Drag
-
 -- Window --
 local UI_SIZE = UDim2.new(0, 420, 0, 260)
 local UI_POSITION = UDim2.new(0.5, -210, 0.5, -130)
@@ -31,3 +29,45 @@ Main.Parent = ZorexUI
 local UIC = Instance.new("UICorner")
 UIC.CornerRadius = UDim.new(0, 12)
 UIC.Parent = Main
+
+-- Universal Drag
+local UserInputService = game:GetService("UserInputService")
+
+local dragging = false
+local dragInput, dragStart, startPos
+
+local function update(input)
+    local delta = input.Position - dragStart
+    Main.Position = UDim2.new(
+        startPos.X.Scale,
+        startPos.X.Offset + delta.X,
+        startPos.Y.Scale,
+        startPos.Y.Offset + delta.Y
+    )
+end
+
+Main.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+        dragging = true
+        dragStart = input.Position
+        startPos = Main.Position
+
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then
+                dragging = false
+            end
+        end)
+    end
+end)
+
+Main.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+        dragInput = input
+    end
+end)
+
+UserInputService.InputChanged:Connect(function(input)
+    if input == dragInput and dragging then
+        update(input)
+    end
+end)
