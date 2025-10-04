750aee3570e35576da72426ababf18cbb0732ceb
Zoprever <zoprever@gmail.com>
2025-10-04 16:46:19 -0700

    Update Scripts.lua

--- Scripts/Scripts.lua
+++ Scripts/Scripts.lua
@@ -1,4 +1,4 @@
 -- Dragify
-loadstring(game:GetHttp("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Scripts/Dragify.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Scripts/Dragify.lua"))()
 -- Notify
-loadstring(game:GetHttp("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Scripts/Notify.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Scripts/Notify.lua"))()
