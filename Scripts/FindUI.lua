f2e842db4fa77f478e25695589648dc9402d790b
Zoprever <zoprever@gmail.com>
2025-10-05 15:13:06 -0700

    Update FindUI.lua

--- Scripts/FindUI.lua
+++ Scripts/FindUI.lua
@@ -1,15 +1,17 @@
-funtion
--- Search UI --
+function SearchForUI(NameUI)
+-- GetParent --
 local parentGui
-if typeof(gethui) == "function" then
-    parentGui = gethui()
-else
-    parentGui = game:GetService("CoreGui")
-end
+    if typeof(gethui) == "function" then
+        parentGui = gethui()
+    else
+        parentGui = game:GetService("CoreGui")
+    end
 
--- Buscar un ScreenGui existente llamado "ZorexUI"
-local ScreenGui = parentGui:FindFirstChild("ZorexUI")
-
--- Si no existe, se crea
-if not ScreenGui then
+    -- Find The ScreenGUI
+    local ScreenGui = parentGui:FindFirstChild(NameUI)
+    
+    -- If It Doesnts Exists Then Mark Print
+    if not ScreenGui then
+        print("Cannot Find: " .. NameUI)
+    end
 end
