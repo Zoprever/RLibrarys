fc48a6cc31c2c959d765a7a488f74caca2688527
Zoprever <zoprever@gmail.com>
2025-10-06 02:08:48 -0700

    Update Scripts.lua

--- Scripts/Scripts.lua
+++ Scripts/Scripts.lua
@@ -2,3 +2,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Scripts/Dragify.lua"))()
 -- Notify
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Scripts/Notify.lua"))()
+loadstring(game:HttpGet("https://github.com/Zoprever/RLibrarys/blob/main/Scripts/FindUI.lua"))()
