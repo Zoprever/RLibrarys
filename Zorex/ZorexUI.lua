08e3198b9bf0cb31cf39ac280ebd65be86b418da
Zoprever <zoprever@gmail.com>
2025-10-06 02:36:10 -0700

    Update ZorexUI.lua

--- Zorex/ZorexUI.lua
+++ Zorex/ZorexUI.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Zorex/Window.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Zorex/AssistiveBall.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoprever/RLibrarys/refs/heads/main/Zorex/AssistantBall.lua"))()
