2e9bbe0258afc6e36270883272264e3dc2865536
Zoprever <zoprever@gmail.com>
2025-10-06 10:34:58 -0700

    Update Notify.lua

--- Scripts/Notify.lua
+++ Scripts/Notify.lua
@@ -1,13 +1,24 @@
 function Notify(title, content, icon, seconds)
     game:GetService("StarterGui"):SetCore("SendNotification", {
-          Title = title,
-          Text = content,
-          Icon = icon,
-          Duration = seconds
-      })
-  end
+            Title = title,
+            Text = content,
+            Icon = icon,
+            Duration = seconds
+        })
 end
-function Request(Content, Seconds, B1, B2, CB)
-  game:GetService("StarterGui"):SetCore("SendNotification", {
+function Request(Content, Seconds, A1, A2, CB1, CB2)
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Request",
+            Text = A1 .. " or " .. A2,
+            Duration = Seconds,
+            Button1 = A1,
+            Button2 = A2,
+            Callback = function(Answer)
+                if Callback == A1 then
+                    CB1
+                elseif Callback == A2 then
+                    CB2
+                end
+            end
       })
 end
