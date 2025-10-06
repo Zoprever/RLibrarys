7cb35b0be79d01c64d7a39819769084ca9627527
Zoprever <zoprever@gmail.com>
2025-10-06 02:28:27 -0700

    Update AssistantBall.lua

--- Zorex/AssistantBall.lua
+++ Zorex/AssistantBall.lua
@@ -1,17 +1,19 @@
 -- Find a ScreenGUI to Connect --
-loadstring(game:HttpGet("https://github.com/Zoprever/RLibrarys/blob/main/Scripts/FindUI.lua"))()
+loadstring(game:HttpGet("https://github.com/Zoprever/RLibrarys/blob/main/Scripts/Scripts.lua"))()
 
 local ScreenGui = SearchForUI("ZorexUI")
--- ✅ Ahora puedes crear tus elementos dentro del ScreenGui
+-- Assistive Ball
 local AB = Instance.new("Frame")
 AB.Name = "AssistiveBall"
-AB.Size = UDim2.new(0, 100, 0, 100)
-AB.Position = UDim2.new(0.5, -210, 0.5, -130)
-AB.BackgroundColor3 = Color3.fromRGB(0,0,0)
-AB.BackgroundTransparency = 0.4
+AB.Size = UDim2.new(0, 65, 0, 65)
+AB.Position = UDim2.new(0, 150, 0, 200)
+AB.BackgroundColor3 = Color3.fromRGB(125,80,255)
+AB.BackgroundTransparency = 0.1
 AB.BorderSizePixel = 0
+AB.AnchorPoint = Vector2.new(0.5, 0.5)
 AB.Parent = ScreenGui
 
 local UICorner = Instance.new("UICorner")
-UICorner.CornerRadius = UDim.new(0, 12)
+UICorner.CornerRadius = UDim.new(1, 0)
 UICorner.Parent = AB
+
