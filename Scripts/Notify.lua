34fe72ebdef5bf47b76c4566e05a9b07d28fccce
Zoprever <zoprever@gmail.com>
2025-10-06 11:36:49 -0700

    Update Notify.lua

--- Scripts/Notify.lua
+++ Scripts/Notify.lua
@@ -6,20 +6,3 @@ function Notify(title, content, icon, seconds)
             Duration = seconds
         })
 end
-function Request(Seconds, A1, A2, CB1, CB2)
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "Request",
-            Text = A1 .. " or " .. A2,
-            Duration = Seconds,
-            Button1 = A1,
-            Button2 = A2,
-            Callback = function(Answer)
-                if Callback == A1 then
-                    CB1()
-                elseif Callback == A2 then
-                    CB2()
-                end
-            end
-            warn("Callback doesnt exist")
-      })
-end
